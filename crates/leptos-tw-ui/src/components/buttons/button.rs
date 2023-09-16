26fe68943080e6cd6c47d02625759da4d7e72487
talves <tony@3alves.com>
2023-09-16 12:51:55 -0700

    update: base class variant

--- crates/leptos-tw-ui/src/components/buttons/button.rs
+++ crates/leptos-tw-ui/src/components/buttons/button.rs
@@ -1,38 +1,13 @@
 use leptos::{ev::MouseEvent, *};
-use std::fmt::{Display, Formatter};
 
 use crate::components::variants::base::ClassVariant;
 use crate::OptionMaybeSignal;
-
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
-pub enum ButtonClassVariant {
-    #[default]
-    Unstyled,
-    Str(&'static str),
-    Vec(&'static Vec<&'static str>),
-}
-
-impl ClassVariant for ButtonClassVariant {
-    fn as_vec(&self) -> Vec<&'static str> {
-        match self {
-            ButtonClassVariant::Unstyled => Vec::<&'static str>::new(), // "".split(" ").collect::<Vec<&str>>(),
-            ButtonClassVariant::Str(x) => x.split(" ").collect::<Vec<&str>>(),
-            ButtonClassVariant::Vec(x) => x.to_vec(),
-        }
-    }
-}
-
-impl Display for ButtonClassVariant {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&ClassVariant::to_string(self))
-    }
-}
 
 #[component]
 pub fn Button<F>(
     cx: Scope,
     on_click: F,
-    #[prop(into, optional)] variant: OptionMaybeSignal<ButtonClassVariant>,
+    #[prop(into, optional)] variant: OptionMaybeSignal<ClassVariant>,
     #[prop(into, optional)] disabled: OptionMaybeSignal<bool>,
     #[prop(into, optional)] id: Option<AttributeValue>,
     #[prop(into, optional)] class: OptionMaybeSignal<String>,
@@ -63,7 +38,7 @@ where
 #[component]
 pub fn LinkButton(
     cx: Scope,
-    #[prop(into, optional)] variant: OptionMaybeSignal<ButtonClassVariant>,
+    #[prop(into, optional)] variant: OptionMaybeSignal<ClassVariant>,
     #[prop(into, optional)] disabled: OptionMaybeSignal<bool>,
     #[prop(into, optional)] id: Option<AttributeValue>,
     #[prop(into, optional)] class: OptionMaybeSignal<String>,
--- site/src/theme.rs
+++ site/src/theme.rs
@@ -1,4 +1,4 @@
-use leptos_tw_ui::components::buttons::button::ButtonClassVariant;
+use leptos_tw_ui::components::variants::base::ClassVariant;
 
 pub enum ButtonVariant {
     Solid,
@@ -10,14 +10,36 @@ pub enum ButtonVariant {
 }
 
 impl ButtonVariant {
-    pub fn get(&self) -> ButtonClassVariant {
+    pub fn get(&self) -> ClassVariant {
+        const SOLID: &'static [&'static str] = &[
+            "py-3",
+            "px-4",
+            "inline-flex",
+            "justify-center",
+            "items-center",
+            "gap-2",
+            "rounded-md",
+            "border",
+            "border-transparent",
+            "font-semibold",
+            "bg-blue-500",
+            "text-white",
+            "hover:bg-blue-600",
+            "focus:outline-none",
+            "focus:ring-2",
+            "focus:ring-blue-500",
+            "focus:ring-offset-2",
+            "transition-all",
+            "text-sm",
+            "dark:focus:ring-offset-gray-800",
+        ];
         match self {
-            ButtonVariant::Solid => ButtonClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"),
-            ButtonVariant::Outline => ButtonClassVariant::Str("py-[.688rem] px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500"),
-            ButtonVariant::Ghost => ButtonClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"),
-            ButtonVariant::Soft => ButtonClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md bg-blue-100 border border-transparent font-semibold text-blue-500 hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"),
-            ButtonVariant::White => ButtonClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"),
-            ButtonVariant::Link => ButtonClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"),
+            ButtonVariant::Solid => ClassVariant::Vec(SOLID),
+            ButtonVariant::Outline => ClassVariant::Str("py-[.688rem] px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500"),
+            ButtonVariant::Ghost => ClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"),
+            ButtonVariant::Soft => ClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md bg-blue-100 border border-transparent font-semibold text-blue-500 hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"),
+            ButtonVariant::White => ClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"),
+            ButtonVariant::Link => ClassVariant::Str("py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"),
         }
     }
 }
